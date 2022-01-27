e245b2f8a1bb2392a4ab463e2f8b6d20ec10216f
Pratik_Tulkane <pratiktulkane12345@gmail.com>
2022-01-27 11:38:05 +0530

    first commit

---
+++ src/Component/Navbar/SampleData.js
@@ -0,0 +1,45 @@
+const Data = [
+    {
+    "coord": {
+      "lon": 79.1,
+      "lat": 21.15
+    },
+    "weather": [
+      {
+        "id": 721,
+        "main": "Haze",
+        "description": "haze",
+        "icon": "50d"
+      }
+    ],
+    "base": "stations",
+    "main": {
+      "temp": 296.16,
+      "feels_like": 296.29,
+      "temp_min": 296.16,
+      "temp_max": 296.16,
+      "pressure": 1016,
+      "humidity": 68
+    },
+    "visibility": 2500,
+    "wind": {
+      "speed": 2.06,
+      "deg": 120
+    },
+    "clouds": {
+      "all": 40
+    },
+    "dt": 1642238018,
+    "sys": {
+      "type": 1,
+      "id": 9069,
+      "country": "IN",
+      "sunrise": 1642209796,
+      "sunset": 1642249323
+    },
+    "timezone": 19800,
+    "id": 1262180,
+    "name": "Nagpur",
+    "cod": 200
+  }
+]
